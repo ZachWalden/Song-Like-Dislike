7fe95d9d439324cc387c31c4f03d92c34d6593df
ZachWalden <zachwalden2@outlook.com>
2024-03-07 21:12:29 -0600

    testing playing file experimenting with recording keyboard inputs

---
+++ MoveSongs.py
@@ -0,0 +1,6 @@
+import keyboard, os, subprocess
+
+path_to_playable_songs = os.path.abspath(r"C:\Users\zachw\Downloads\AxS Comp")
+path_to_liked_songs = os.path.abspath(r"C:\Users\zachw\Desktop\songs")
+
+os.startfile(r'C:\Users\zachw\Desktop\songs\500 Carats.mp3')
